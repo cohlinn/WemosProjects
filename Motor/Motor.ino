6216b838b920faf590a7565629f4a7f7606b3b57
Cathy Ooi <cathy001@gmail.com>
2017-02-25 17:54:56 +0800

    add directions

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -1,30 +1,81 @@
-int mPin1 = D1;
-int mPin2 = D2;
+int mA1 = D1;
+int mA2 = D2;
+int mB1 = D5;
+int mB2 = D6;
 int mSpeed = D3;
 
 void setup() 
 {
   Serial.begin(115200);
   //Setup pins for motor control
-  pinMode(mPin1, OUTPUT);
-  pinMode(mPin2, OUTPUT);
-  digitalWrite(mPin1, LOW);
-  digitalWrite(mPin2, LOW);
+  pinMode(mA1, OUTPUT);
+  pinMode(mA2, OUTPUT);
+  pinMode(mB1, OUTPUT);
+  pinMode(mB2, OUTPUT);
+  digitalWrite(mA1, LOW);
+  digitalWrite(mA2, LOW);
+  digitalWrite(mB1, LOW);
+  digitalWrite(mB2, LOW);
   //set speed
   analogWrite(mSpeed, 700);
 }
 
 void loop() {
-  Serial.println("Move 1");
-  digitalWrite(mPin1, HIGH);
-  digitalWrite(mPin2, LOW);
-  delay(1000);
-  Serial.println("Move 2");
-  digitalWrite(mPin1, LOW);
-  digitalWrite(mPin2, HIGH);
-  delay(1000);
-  Serial.println("Stop");
-  digitalWrite(mPin1, LOW);
-  digitalWrite(mPin2, LOW);
-  delay(1000);
+  int interval = 2000;
+  moveFwd();
+  delay(interval);
+  moveStop();
+  delay(interval);
+  moveBwd();
+  delay(interval);
+  moveStop();
+  delay(interval);
+  moveLeft();
+  delay(interval);
+  moveStop();
+  delay(interval);
+  moveRight();
+  delay(interval);
+  moveStop();
+  delay(interval);
+}
+
+void moveFwd()
+{
+  digitalWrite(mA1, HIGH);
+  digitalWrite(mA2, LOW);
+  digitalWrite(mB1, HIGH);
+  digitalWrite(mB2, LOW);
+}
+
+void moveBwd()
+{
+  digitalWrite(mA1, LOW);
+  digitalWrite(mA2, HIGH);
+  digitalWrite(mB1, LOW);
+  digitalWrite(mB2, HIGH);
+}
+
+void moveRight()
+{
+  digitalWrite(mA1, HIGH);
+  digitalWrite(mA2, LOW);
+  digitalWrite(mB1, LOW);
+  digitalWrite(mB2, HIGH);
+}
+
+void moveLeft()
+{
+  digitalWrite(mA1, LOW);
+  digitalWrite(mA2, HIGH);
+  digitalWrite(mB1, HIGH);
+  digitalWrite(mB2, LOW);
+}
+
+void moveStop()
+{
+  digitalWrite(mA1, LOW);
+  digitalWrite(mA2, LOW);
+  digitalWrite(mB1, LOW);
+  digitalWrite(mB2, LOW);
 }
