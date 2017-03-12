6bc71b579ea8c4e941d8a25ba04e96154fffaff1
Cathy Ooi <cathy001@gmail.com>
2017-03-12 13:56:57 +0800

    Combine direction controls for wifi

--- MotorWifi/MotorWifi.ino
+++ MotorWifi/MotorWifi.ino
@@ -1,21 +1,17 @@
 #include <Wire.h>
 #include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 #include "images.h" //WIFI logo
-#include <NewPing.h>
+//#include <NewPing.h>
+//
+//#define TRIGGER_PIN  D6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+//#define ECHO_PIN     D7  // Arduino pin tied to echo pin on the ultrasonic sensor.
+//#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//
+//// Initialize the OLED display using Wire library
+SSD1306  display(0x3c, D3, D4); //D3=SDA, D4=SCL
 
-#define TRIGGER_PIN  D6  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     D7  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
-// Initialize the OLED display using Wire library
-SSD1306  display(0x3c, D3, D5); //D3=SDA, D5=SCL
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
 
 
 /* Create a WiFi access point and provide a web server on it. */
@@ -30,18 +26,38 @@ const char WiFiSSID[] = "CATHY";
 const char WiFiAPPSK[] = "mysecret";
 ESP8266WebServer server(80);
 String AP_NameString;
+boolean APMODE = false; //false = connect to Wifi, true = setup Wifi Access Point (IP=192.168.4.1)
+//config for public wifi
+const char ssid[] = "pepsi wifi";
+const char password[] = "Gandalf3062";
 
-int mPin1 = D1;
-int mPin2 = D2;
+
+int mR1 = D5;
+int mR2 = D6;
+int mL1 = D1;
+int mL2 = D2;
+int mSpeed = A0;
+#define LED1  D7
+#define LED2  D8
+int currSpeed = 600;
+String mStatus = "Status.....";
 
 void setup() 
 {
   Serial.begin(115200);
   //Setup pins for motor control
-  pinMode(mPin1, OUTPUT);
-  pinMode(mPin2, OUTPUT);
-  digitalWrite(mPin1, LOW);
-  digitalWrite(mPin2, LOW);
+  pinMode(mR1, OUTPUT);
+  pinMode(mR2, OUTPUT);
+  digitalWrite(mR1, LOW);
+  digitalWrite(mR2, LOW);
+
+  pinMode(mL1, OUTPUT);
+  pinMode(mL2, OUTPUT);
+  digitalWrite(mL1, LOW);
+  digitalWrite(mL2, LOW);
+
+  analogWrite(mSpeed, 0); //0 - 1023 speed. 0 - stop
+  mStatus = "Stop";
 
     // Initialising the UI will init the display too.
 
@@ -56,8 +72,10 @@ void setup()
     //Setup Wifi AP
   setupWiFi();
   server.on("/", handleRoot);
-  server.on("/up", handleUp);
-  server.on("/down", handleDown);
+  server.on("/fwd", handleFwd);
+  server.on("/back", handleBack);
+  server.on("/right", handleRight);
+  server.on("/left", handleLeft);
   server.on("/stop", handleStop);
   server.begin();
   Serial.println("HTTP server started");
@@ -67,47 +85,51 @@ void loop() {
 // Check if a client has connected
   server.handleClient();
 
-  int value = sonar.ping_cm();
-
-  Serial.print("Ping: ");
-  Serial.print(value); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  Serial.println(" cm");
-
   display.clear();
-  drawOledText(value);
+  drawOledText(mStatus);
   display.display();
-
-  //motor section
-  if(value <10)
-  {
-    value = sonar.ping_cm();
-    if(value <10) moveStop(5);
-  }
+//  Serial.println(mStatus);
 
 }
 
 
 void setupWiFi()
 {
-  WiFi.mode(WIFI_AP);
-
-  // Do a little work to get a unique-ish name. Append the
-  // last two bytes of the MAC (HEX'd) to "Thing-":
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  AP_NameString = WiFiSSID + macID;
-
-  Serial.println("Configuring access point "+AP_NameString);
-  char APchar[AP_NameString.length()+1];
-  AP_NameString.toCharArray(APchar, AP_NameString.length());
+  if(APMODE)
+  {
+    WiFi.mode(WIFI_AP);
   
-  WiFi.softAP(APchar, WiFiAPPSK);
-  IPAddress myIP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(myIP);
+    // Do a little work to get a unique-ish name. Append the
+    // last two bytes of the MAC (HEX'd) to "Thing-":
+    uint8_t mac[WL_MAC_ADDR_LENGTH];
+    WiFi.softAPmacAddress(mac);
+    String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                   String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+    macID.toUpperCase();
+    AP_NameString = WiFiSSID + macID;
+  
+    Serial.println("Configuring access point "+AP_NameString);
+    char APchar[AP_NameString.length()+1];
+    AP_NameString.toCharArray(APchar, AP_NameString.length());
+    
+    WiFi.softAP(APchar, WiFiAPPSK);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+  }
+  else
+  {
+     WiFi.begin(ssid, password);
+     while (WiFi.status() != WL_CONNECTED) 
+     {
+      delay(500);
+      Serial.print(".");
+     }
+     Serial.println("");
+     Serial.println("WiFi connected");  
+     Serial.println("IP address: ");
+     Serial.println(WiFi.localIP());
+  }
 }
 
 
@@ -119,27 +141,59 @@ const String MIME_TEXT = "text/plain";
 void handleRoot() {
   server.send(200, MIME_HTML, 
     "<b>You are connected to "+AP_NameString+"</b>"
-    + "<p><a href='/up'><button type='button' value='Move Up'>Move Up</button></a></p>"
-    + "<p><a href='/down'><button type='button' value='Move Down'>Move Down</button></a></p>"
+    + "<p><a href='/fwd'><button type='button' value='Forward'>Forward</button></a></p>"
+    + "<p><a href='/left'><button type='button' value='Left'>Left</button></a>"
+    +    "<a href='/right'><button type='button' value='Right'>Right</button></a></p>"
+    + "<p><a href='/back'><button type='button' value='back'>Back</button></a></p>"
     + "<p><a href='/stop'><button type='button' value='Stop'>Stop</button></a></p>");
 }
 
-void handleUp() 
+void handleFwd() 
 {
   //return no content
-  server.send(204, MIME_TEXT, "");
-  Serial.println("Move 1");
-  digitalWrite(mPin1, HIGH);
-  digitalWrite(mPin2, LOW);
+  server.send(204, MIME_TEXT, "Forward");
+  Serial.println("Forward");
+  digitalWrite(mR1, HIGH);
+  digitalWrite(mR2, LOW);
+  digitalWrite(mL1, HIGH);
+  digitalWrite(mL2, LOW);
+  analogWrite(mSpeed, currSpeed);
 }
 
-void handleDown() 
+void handleBack() 
 {
   //return no content
   server.send(204, MIME_TEXT, "");
-  Serial.println("Move 2");
-  digitalWrite(mPin1, LOW);
-  digitalWrite(mPin2, HIGH);
+  Serial.println("Back");
+  digitalWrite(mR1, LOW);
+  digitalWrite(mR2, HIGH);
+  digitalWrite(mL1, LOW);
+  digitalWrite(mL2, HIGH);
+  analogWrite(mSpeed, currSpeed);
+}
+
+void handleLeft() 
+{
+  //return no content
+  server.send(204, MIME_TEXT, "");
+  Serial.println("Left");
+  digitalWrite(mR1, HIGH);
+  digitalWrite(mR2, LOW);
+  digitalWrite(mL1, LOW);
+  digitalWrite(mL2, HIGH);
+  analogWrite(mSpeed, currSpeed);
+}
+
+void handleRight() 
+{
+  //return no content
+  server.send(204, MIME_TEXT, "");
+  Serial.println("Right");
+  digitalWrite(mR1, LOW);
+  digitalWrite(mR2, HIGH);
+  digitalWrite(mL1, HIGH);
+  digitalWrite(mL2, LOW);
+  analogWrite(mSpeed, currSpeed);
 }
 
 void handleStop() 
@@ -147,25 +201,23 @@ void handleStop()
   //return no content
   server.send(204, MIME_TEXT, "");
   Serial.println("Stop");
-  digitalWrite(mPin1, LOW);
-  digitalWrite(mPin2, LOW);
+  analogWrite(mSpeed, 0);
+  digitalWrite(mR1, LOW);
+  digitalWrite(mR2, LOW);
+  digitalWrite(mL1, LOW);
+  digitalWrite(mL2, LOW);
 }
 
-void moveStop(int value)
-{
-   digitalWrite(mPin1, LOW);
-   digitalWrite(mPin2, LOW);
-}
 
-void drawOledText(int value) {
+void drawOledText(String value) {
     // create more fonts at http://oleddisplay.squix.ch/
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 0, "Wemos Cathy-01");
+    display.drawString(0, 0, "Wemos "+WiFi.localIP());
     display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 24, "Distance:");
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(30, 35, String(value) + " cm");
+//    display.drawString(0, 24, mStatus);
+//    display.setFont(ArialMT_Plain_10);
+//    display.drawString(30, 35, "Speed:" + currSpeed);
 }
 
 void drawWifi() {
