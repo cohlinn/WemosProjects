d8b5c85d2c0d3d18fd681547050e7e4fa5de1f6b
Cathy Ooi <cathy001@gmail.com>
2018-11-01 08:32:46 +0800

    Flash ESP-01 with Wemos D1 Mini

---
+++ FlashESP01/FlashESP01.ino
@@ -0,0 +1,29 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
+  of use the correct LED pin whatever is the board used.
+  If you want to know what pin the on-board LED is connected to on your Arduino model, check
+  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
+  
+    This example code is in the public domain.
+*/
+
+//#define LED  0//GPIO 0 on ESP-01, D3 on Wemos
+#define LED  2//GPIO 2 on ESP-01, D4 on Wemos
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
--- Neopixels/Neopixels.ino
+++ Neopixels/Neopixels.ino
@@ -1,9 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
-#define NUM_PIXELS 21
+#define NUM_PIXELS 10
 #define NEOPIXEL_PIN D7
-//Adafruit_NeoPixel neopix(NUM_PIXELS, NEOPIXEL_PIN, NEO_RGB | NEO_KHZ800);
-Adafruit_NeoPixel neopix(NUM_PIXELS, NEOPIXEL_PIN, NEO_RGBW | NEO_KHZ800);
+Adafruit_NeoPixel strip(NUM_PIXELS, NEOPIXEL_PIN, NEO_RGB | NEO_KHZ800);
+//Adafruit_NeoPixel strip(NUM_PIXELS, NEOPIXEL_PIN, NEO_RGBW | NEO_KHZ800);
 
 //TO BE MODIFIED according to need
 //Parameters for Solid display
@@ -28,56 +28,72 @@ int gHead = 0;
 void setup() 
 {
   Serial.begin(115200);
-  neopix.begin();
+  strip.begin();
 
-  //SolidColour sections
-  solidIndex=0;
-
-  //GradualColour sections
-  gHead=0;
-  setupGradPixels(NUM_PIXELS, 0x00, 0x00, 0x00);
-
-  //Setup MRT lights (4 white, the rest blue)
-  setupGradPixels(NUM_PIXELS, 0x05, 0x05, 0x0a);
-  for(int i=0; i<4; i++)
-    setGradPixel(i, 0x05, 0x05, 0x05);
+//  //SolidColour sections
+//  solidIndex=0;
+//
+//  //GradualColour sections
+//  gHead=0;
+//  setupGradPixels(NUM_PIXELS, 0x00, 0x00, 0x00);
+//
+//  //Setup MRT lights (4 white, the rest blue)
+//  setupGradPixels(NUM_PIXELS, 0x05, 0x05, 0x0a);
+//  for(int i=0; i<4; i++)
+//    setGradPixel(i, 0x05, 0x05, 0x05);
 }
 
 void loop() 
 {
 
-  /* Start SolidColour sections
-  solidColorCycle(NUM_PIXELS, solidColours, SOLID_LIST_LEN);
-  // End SolidColor Sections */
+  rainbowCycle(20);
 
-  /* Start GradualColour section
-  byte r, g, b;
-  r = random(256);
-  g = random(256);
-  b = random(256);
-  setGradPixel(gHead, r, g, b);
-  Serial.printf("%d: %d %d %d \n", gHead, r, g, b);
-  */
-
-  gradColorCycle();
-  // End GradualColour section */
-  neopix.show();
+//  strip.show();
   delay(100);
 }
 
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
 //==== Gradual Colour section
-void gradColorCycle()
+/*void gradColorCycle()
 {
  int j = gHead;
   for(int i=0; i<NUM_PIXELS; i++, j++)
   {//i=neopixel position, j=colour to assign to that pixel from gradColours
-    j = j % NUM_PIXELS;
+    j = j % SOLID_LIST_LEN;
     setColor(i,gradColours[j][0], gradColours[j][1], gradColours[j][2]);
   }
   gHead = abs(gHead+1) % NUM_PIXELS;
 }
 
-
+*/
 void setupGradPixels(int numpixels, byte r, byte g, byte b)
 {
   for(int i=0; i<numpixels; i++)
@@ -95,43 +111,6 @@ void setGradPixel(int pos, byte r, byte g, byte b)
 
 
 //======== Common functions
-//set a single pixel
-void setColor(int pos, byte red, byte green, byte blue) 
-{
-  byte white = 0;
-  if(red && green && blue)
-  {
-    white = min(red, green);
-    white = min(blue, white);
-    red = red - white;
-    green = green - white;
-    blue = blue - white;
-  }
-  neopix.setPixelColor(pos, neopix.Color(red, green, blue, white));
-}
 
 
-//====== Functions for Solid Colours
-/*
-  Code for controling Neopixels.
-  This sets the all neopixels into the same colour.
-  The next time it is called, it will set all neopixels to the next colour on the list.
-  The list of colours and its length is defined by the user.
-  Created by Cathy Ooi, 2016-10-07.
-  Released into the public domain.
-*/
-
-void setAllSameColor(int numpixels, byte *rgb)
-{
-  for(int i=0; i<numpixels; i++)
-  {
-    setColor(i, rgb[0], rgb[1], rgb[2]);
-  }
-}
-
-void solidColorCycle(int numpixels, byte colorList[][3], int colorListLen)
-{
-  setAllSameColor(numpixels, colorList[solidIndex]);
-  solidIndex = (solidIndex+1) % colorListLen;
-}
-
+\
