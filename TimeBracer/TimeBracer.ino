935c1527f752ac58147e5b82ad5b12f8f9676be3
Cathy Ooi <cathy001@gmail.com>
2019-01-08 00:08:04 +0800

    Lower brightness of Wheel. Change default pattern to RainbowCycle.

--- TimeBracer/TimeBracer.ino
+++ TimeBracer/TimeBracer.ino
@@ -53,7 +53,7 @@ void setup() {
 
 }
 
-int show=0;
+int show=9; //default pattern
 int cycle=0;
 
 void loop() {
@@ -154,11 +154,13 @@ void colorWipe(uint32_t c, uint8_t wait) {
   }
 }
 
+
 void rainbow(uint8_t wait) {
-  uint16_t i, j;
+uint16_t i, j;
 
   for(j=0; j<256; j++) {
     for(i=0; i<strip.numPixels(); i++) {
+      //strip.setPixelColor(i, Wheel((i+j) & 255));
       strip.setPixelColor(i, Wheel((i+j) & 255));
     }
     strip.show();
@@ -222,15 +224,16 @@ void theaterChaseRainbow(uint8_t wait) {
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
+  int brightness = 50; //default 5; lower=brighter
   if(WheelPos < 85) {
-    return strip.Color((255 - WheelPos * 3)/5, 0, (WheelPos * 3)/5);
+    return strip.Color((255 - WheelPos * 3)/brightness, 0, (WheelPos * 3)/brightness);
   }
   if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, (WheelPos * 3)/5, (255 - WheelPos * 3)/5);
+    return strip.Color(0, (WheelPos * 3)/brightness, (255 - WheelPos * 3)/brightness);
   }
   WheelPos -= 170;
-  return strip.Color((WheelPos * 3)/5, (255 - WheelPos * 3)/5, 0);
+  return strip.Color((WheelPos * 3)/brightness, (255 - WheelPos * 3)/brightness, 0);
 }
 
 
